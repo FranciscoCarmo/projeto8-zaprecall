caf3eaea8e01c36f316e4e91be593db3d929d080
Francisco Carmo <franciscoacarmo@gmail.com>
2022-07-20 15:06:54 -0300

    Adiciona imagens a pasta

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import Start from "./Start";
+
+export default function App() {
+  return <Start />;
+}
---
+++ src/Start.js
@@ -0,0 +1,9 @@
+export default function Start() {
+  return (
+    <div className="firstPage">
+      <img className="logo" src="images/logo"></img>
+      <h1 className="title">ZapRecall</h1>
+      <div className="startButton">Iniciar Recall!</div>
+    </div>
+  );
+}
