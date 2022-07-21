7a5de5142f884007b453d48ac501219ea14c5dce
Francisco Carmo <franciscoacarmo@gmail.com>
2022-07-21 15:52:09 -0300

    integra a tela inicial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React from "react";
 import Start from "./Start";
 import QuestionsPage from "./QuestionsPage";
 
 export default function App() {
-  return <QuestionsPage />;
+  const [startRecall, setStartRecall] = React.useState(false);
+
+  if (startRecall) {
+    return <QuestionsPage />;
+  } else {
+    return <Start setStartRecall={setStartRecall} />;
+  }
 }
--- src/Start.js
+++ src/Start.js
@@ -1,11 +1,13 @@
-export default function Start() {
+export default function Start({ setStartRecall }) {
   return (
     <div className="firstPage">
       <div className="logo">
         <img src="images/logo.png"></img>
       </div>
       <h1 className="title">ZapRecall</h1>
-      <div className="startButton">Iniciar Recall!</div>
+      <div className="startButton" onClick={() => setStartRecall(true)}>
+        Iniciar Recall!
+      </div>
     </div>
   );
 }
