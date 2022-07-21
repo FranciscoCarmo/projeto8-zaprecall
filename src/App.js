9d42edc4742a7971949adebe9935366d88238461
Francisco Carmo <franciscoacarmo@gmail.com>
2022-07-21 18:22:25 -0300

    adiciona o bonus de escolher o deck

--- src/App.js
+++ src/App.js
@@ -3,15 +3,150 @@ import Start from "./Start";
 import QuestionsPage from "./QuestionsPage";
 
 export default function App() {
+  const questionListReact = [
+    {
+      Q: "O que é JSX?",
+      R: "Uma extensão de linguagem do JavaScript",
+    },
+    {
+      Q: "O React é __ ",
+      R: "uma biblioteca JavaScript para construção de interfaces",
+    },
+    {
+      Q: "Componentes devem iniciar com __ ",
+      R: "letra maiúscula",
+    },
+    {
+      Q: "Podemos colocar __ dentro do JSX ",
+      R: "expressões",
+    },
+    {
+      Q: "O ReactDOM nos ajuda __",
+      R: "interagindo com a DOM para colocar componentes React na mesma",
+    },
+    {
+      Q: "Usamos o npm para __ ",
+      R: "gerenciar os pacotes necessários e suas dependências",
+    },
+    {
+      Q: "Usamos props para __ ",
+      R: "passar diferentes informações para componentes ",
+    },
+    {
+      Q: "Usamos estado (state) para __",
+      R: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+    },
+  ];
+
+  const questionListAnimais = [
+    {
+      Q: "Qual o maior felino do mundo, em dimensões?",
+      R: "Tigre",
+    },
+    {
+      Q: "Qual o maior réptil do mundo?",
+      R: "Cobra",
+    },
+    {
+      Q: "Qual a maior ave do mundo?",
+      R: "Avestruz",
+    },
+    {
+      Q: "Qual o maior animal vivo no planeta? ",
+      R: "Baleia Azul",
+    },
+    {
+      Q: "Quе аnіmаl é еѕѕе? О tаmаnhо vаrіа еntrе 3 mm а 10 сm dе соmрrіmеntо dереndеndо dа еѕрéсіе. Теm аntеnаѕ lоngаѕ, trêѕ раrеѕ dе реrnаѕ, dоіѕ раrеѕ dе аѕаѕ.",
+      R: "Barata",
+    },
+    {
+      Q: "Qual a maior cobra do mundo?",
+      R: "Píton",
+    },
+    {
+      Q: "Como se chamam as espécies que podem viver tanto em terra quanto na água?",
+      R: "Anfíbios ",
+    },
+    {
+      Q: "Qual o animal mais venenoso do mundo?",
+      R: "Aranha",
+    },
+  ];
+
+  const questionListEsportes = [
+    {
+      Q: "Quanto tempo dura uma partida de futebol?",
+      R: "90 minutos, divididos em duas partes de 45 minutos cada.      ",
+    },
+    {
+      Q: "Quando foi realizada a primeira Copa do Mundo de futebol?",
+      R: "Em 13 de julho de 1930, no Uruguai. ",
+    },
+    {
+      Q: "Qual time de futebol ganhou mais Copas do Mundo?",
+      R: "Brasil (5).",
+    },
+    {
+      Q: "Qual é o apelido de Leicester City? ",
+      R: "The Foxes",
+    },
+    {
+      Q: "Quantos jogadores um time de futebol tem em jogo? ",
+      R: " 11",
+    },
+    {
+      Q: "Quem ganhou a Copa do Mundo de 2010?",
+      R: "A seleção da Espanha.",
+    },
+    {
+      Q: "Quem é o artilheiro do FC Barcelona? ",
+      R: "Atualmente, Lionel Messi.",
+    },
+    {
+      Q: "Em que time italiano jogou Diego Maradona? ",
+      R: "No Napoli.",
+    },
+  ];
+
+  const arrayDecks = [
+    {
+      value: questionListReact,
+      label: "React",
+    },
+
+    {
+      value: questionListAnimais,
+      label: "Animais",
+    },
+
+    {
+      value: questionListEsportes,
+      label: "Esportes",
+    },
+  ];
+
   const [startRecall, setStartRecall] = React.useState(false);
+
+  const [deckSelected, setdeckSelected] = React.useState(questionListReact);
 
   function resetStartRecall() {
     setStartRecall(false);
   }
 
   if (startRecall) {
-    return <QuestionsPage resetStartRecall={resetStartRecall} />;
+    return (
+      <QuestionsPage
+        resetStartRecall={resetStartRecall}
+        deckSelected={deckSelected}
+      />
+    );
   } else {
-    return <Start setStartRecall={setStartRecall} />;
+    return (
+      <Start
+        setStartRecall={setStartRecall}
+        arrayDecks={arrayDecks}
+        setdeckSelected={setdeckSelected}
+      />
+    );
   }
 }
--- src/Start.js
+++ src/Start.js
@@ -1,10 +1,21 @@
-export default function Start({ setStartRecall }) {
+import Select from "react-select";
+
+export default function Start({ setStartRecall, arrayDecks, setdeckSelected }) {
+  function handleAddrTypeChange(e) {
+    setdeckSelected(e.value);
+  }
+
   return (
     <div className="firstPage">
       <div className="logo">
         <img src="images/logo.png"></img>
       </div>
       <h1 className="title">ZapRecall</h1>
+      <Select
+        className="select"
+        options={arrayDecks}
+        onChange={(e) => handleAddrTypeChange(e)}
+      />
       <div className="startButton" onClick={() => setStartRecall(true)}>
         Iniciar Recall!
       </div>
